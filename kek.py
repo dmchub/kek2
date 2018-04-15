61eb244ff902c34ee5169ed0c8327b2a260a753a
dmchub <dschub@sfedu.ru>
2018-04-15 16:37:44 +0300

    'correct'

--- kek.py
+++ kek.py
@@ -6,74 +6,49 @@ a = b'NTYyNDA1ODEzOkFBRVdVSW1qekR3YTRGOXV3dzg4UUdJMDZuUUl3Ti1ZZmNJ'
 b = base64.b64decode(a).decode("utf-8", "ignore")
 url = "https://api.telegram.org/bot{}/".format(b)
 
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
-
 def get_updates_json(request):  
-    params = {'timeout': 100, 'offset': None}
-    response = requests.get(request + 'getUpdates', data=params)
-    return response.json()
+	params = {'timeout': 100, 'offset': None}
+	response = requests.get(request + 'getUpdates', data=params)
+	return response.json()
 
 
 def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-    
-    
+	results = data['result']
+	total_updates = len(results) - 1
+	return results[total_updates]
+	
+	
 def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
+	chat_id = update['message']['chat']['id']
+	return chat_id
 
 
-def get_chat_text(chat, text):
-    chat_text = update['message']['text']
-    return chat_text
+def get_member_name(update):
+	member = update["message"]['new_chat_member']['first_name']
+	return member
 
 
 def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+	params = {'chat_id': chat, 'text': text}
+	response = requests.post(url + 'sendMessage', data=params)
+	return response
 
+	
 def main():
-    data = get_updates_json(url)
-    results = data['result']
-    index = len(results) - 1
-    member = results[index]["message"]['new_chat_member']['first_name']
-    chat_id = results[index]['message']['chat']['id']
-    chat_name = results[index]['message']['chat']['title']
-    """
-    print(index)
-    print('Привет {}'.format(member))
-    print('Номер чатика: {}, название: {}'.format(chat_id, chat_name))
+	update_id = last_update(get_updates_json(url))['update_id']
+	new_member = 'new_chat_member'
+	while True:
+		if update_id == last_update(get_updates_json(url))['update_id']:
+			message = last_update(get_updates_json(url))['message']
+			keys = message.keys()
+			if new_member in keys:
+				name = get_member_name(last_update(get_updates_json(url)))
+				send_mess(get_chat_id(last_update(get_updates_json(url))), 'Приветствую, {}! \n\nЭто группа Нассима Талеба.\nЗдесь собираются охотники за счастливыми лебедями, жертвы ассиметрии, жадные читатели и просто антихрупкие люди. \n\n Наш канал \n https://t.me/nntaleb \n\n Канал в контакте \n https://vk.com/nntaleb \n\n Причитать новую книгу Skin in the Game на английском \n https://t.me/nntaleb_group/714 \n\n Прочитать старую книгу Dynamic Hedging на английском \n https://t.me/nntaleb_group/690 \n\n Получить прокси для Телеграмма \n https://t.me/nntaleb_group/747 \n\n Как настроить доступ к Телеграмму через Tor \n https://t.me/nntaleb_group/770  \n\n Скачать книги Нассима Талеба Вы можете здесь \n https://rutracker.org/forum/tracker.php?nm=%D0%BD%D0%B0%D1%81%D1%81%D0%B8%D0%BC%20%D1%82%D0%B0%D0%BB%D0%B5%D0%B1 \n\n'.format(name))
+			else:
+				send_mess(get_chat_id(last_update(get_updates_json(url))), 'Саудовская Аравия должна быть разрушена!!! Kapish???')
+			update_id += 1
+		sleep(1)       
 
-    
-    if chat_text in greetings:
-        send_mess(get_chat_id(last_update(get_updates_json(url))), 'чо бля')
-    else:
-        send_mess(get_chat_id(last_update(get_updates_json(url))), 'не понимать')
-    
-    
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        last_chat_text = last_update(get_updates_json(url))['text']
-        if last_chat_text in greetings and update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'чо бля')
-           update_id += 1
-        elif update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'привет :)')
-           update_id += 1
-        sleep(1)       
-    """
-
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           # print('Привет {} \nДобро пожаловать в чат. \nПредставься, пожалуйста и расскажи немного о себе, если не сложно'.format(member))
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'Привет {} \nДобро пожаловать в чат. \nПредставься, пожалуйста и расскажи немного о себе, если не сложно'.format(member))
-           update_id += 1
-        sleep(1)       
-    
 
 if __name__ == '__main__':  
-    main()
+	main()
